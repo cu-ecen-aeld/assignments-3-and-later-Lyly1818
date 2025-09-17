8947e34eb93c3ed4563c7d9337d4504ce63c6a9f
Lyly1818 <blaiselesly2@gmail.com>
2025-09-17 17:23:06 -0400

    writer.c updated

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -6,76 +6,63 @@
 #include <fcntl.h>
 #include <syslog.h>
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-    //initializing syslog
     openlog("writer", LOG_PID, LOG_USER);
 
-    //check if exactly 2 arguments are passed
-    if (argc  != 3)
-    {
-        syslog(LOG_ERR, "Errors: 2 arguments are required");
+    if (argc != 3) {
+        syslog(LOG_ERR, "Error: 2 arguments are required");
+        fprintf(stderr, "Error: 2 arguments are required\n");
         closelog();
         return 1;
     }
 
-    //storing arguments
     char *writerfile = argv[1];
     char *writerstr = argv[2];
 
-
-    //check if either of the arguments are empty
-    if (strlen(writerfile) == 0 || strlen(writerstr) == 0)
-    {
-        syslog(LOG_ERR, "Arguments connanr be empty");
+    if (strlen(writerfile) == 0 || strlen(writerstr) == 0) {
+        syslog(LOG_ERR, "Error: Arguments cannot be empty");
+        fprintf(stderr, "Error: Arguments cannot be empty\n");
         closelog();
         return 1;
     }
 
-
-
-//Log the writing operations
     syslog(LOG_DEBUG, "Writing file %s and %s", writerfile, writerstr);
+    fprintf(stderr, "Writing file %s and %s\n", writerfile, writerstr);
 
-    int fd = open(writerfile, O_CREAT | O_WRONLY | O_TRUNC , 0644);
-    //handling failure
-    if (fd == -1)
-    {
-        syslog(LOG_ERR, "Errors: System failed to create or write file: %s", writerfile);
+    int fd = open(writerfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (fd == -1) {
+        syslog(LOG_ERR, "Error: Failed to create or write file: %s", writerfile);
+        fprintf(stderr, "Error: Failed to create or write file: %s\n", writerfile);
         closelog();
         return 1;
     }
 
     size_t str_len = strlen(writerstr);
-    ssize_t bytes_written = write( fd, writerstr, str_len);
-    if (bytes_written == -1)
-    {
-       syslog(LOG_ERR, "Errors: System failed to write to file: %s", writerfile);
+    ssize_t bytes_written = write(fd, writerstr, str_len);
+    if (bytes_written == -1) {
+        syslog(LOG_ERR, "Error: Failed to write to file: %s", writerfile);
+        fprintf(stderr, "Error: Failed to write to file: %s\n", writerfile);
         closelog();
         return 1;
     }
 
-    //check if all bytes were written
-    if ((size_t)bytes_written != str_len)
-    {
-//        syslog(LOG_ERR, "Errors Partial wrte to file: %s -> wrote %zd of %zu bytes");
-        
+    if ((size_t)bytes_written != str_len) {
+        syslog(LOG_ERR, "Error: Partial write to file: %s -> wrote %zd of %zu bytes",
+               writerfile, bytes_written, str_len);
+        fprintf(stderr, "Error: Partial write to file: %s -> wrote %zd of %zu bytes\n",
+                writerfile, bytes_written, str_len);
         closelog();
         return 1;
     }
 
-    //closing the file descriptor
-
-    // close(fd);
-    //handling failure
-    
-    if (close(fd)== -1)
-    {
-        syslog(LOG_ERR, "Failed to close");
+    if (close(fd) == -1) {
+        syslog(LOG_ERR, "Error: Failed to close file %s", writerfile);
+        fprintf(stderr, "Error: Failed to close file %s\n", writerfile);
         closelog();
         return 1;
     }
+
     closelog();
-
     return 0;
 }
